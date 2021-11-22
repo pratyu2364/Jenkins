6e2b0da19408fe0f4204f22abc7c74507a96643f
pratyu2364 <57257938+pratyu2364@users.noreply.github.com>
2021-11-22 19:00:30 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -2,21 +2,17 @@
 # Test case for adding two numbers
 import unittest
 
-from Prog1 import summation, multiply
+from Prog1 import multiply
 
 class TestSum(unittest.TestCase):
     def test_list_int(self):
         """
         Test case to add two numbers
         """
-        data = [23, 31]
-        result = summation(data)
-        self.assertEqual(result, 54)
         x = 20
         y = 40
         result = multiply(x,y)
         self.assertEqual(result, 800)
-        print("Test 2 completed")
         x = 0
         y = 451
         result = multiply(x,y)
@@ -25,6 +21,14 @@ class TestSum(unittest.TestCase):
         y = 12
         result = multiply(x,y)
         self.assertEqual(result, 144)
+        x = 6
+        y = 5
+        result = multiply(x,y)
+        self.assertEqual(result, 30)
+        x = 11
+        y = 5
+        result = multiply(x,y)
+        self.assertEqual(result, 55)
 
 
 if __name__ == '__main__':
